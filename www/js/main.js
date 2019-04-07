1064a0437d2c33399efde6f63faeba4871d3bf4b
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-04-06 23:08:38 -0500

    add pose recog

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,4 @@
-const videoSrc;
+var videoSrc;
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
@@ -9,5 +9,21 @@ if (navigator.mediaDevices.getUserMedia) {
     .catch(function (err0r) {
       console.log("Something went wrong!");
     });
+}
+
+function setup(){
+  var poses = []
+  var posenet = ml5.poseNet(video, modelReady);
+
+  poseNet.on('pose', function (results) {
+    poses = results;
+  });
+}
+
+function run() {
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoint
+    console.log("nose: " + poses[i].pose.keypoints[0]);
+      //DO SOMETHING THREEJS
 }
 
